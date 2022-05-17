cc05f3875d66b923ed253d9daa226ee893b8cbb9
lmphatsi <la.mphatsi@gmail.com>
2022-05-17 10:56:21 +0300

    test commit to repo

--- java/hapi-fhirstarters-simple-server/src/main/java/ca/uhn/fhir/example/Example01_PatientResourceProvider.java
+++ java/hapi-fhirstarters-simple-server/src/main/java/ca/uhn/fhir/example/Example01_PatientResourceProvider.java
@@ -13,35 +13,35 @@ import java.util.Map;
 
 public class Example01_PatientResourceProvider implements IResourceProvider {
 
-   private Map<String, Patient> myPatients = new HashMap<String, Patient>();
+	private Map<String, Patient> myPatients = new HashMap<String, Patient>();
 
-   /**
-    * Constructor
-    */
-   public Example01_PatientResourceProvider() {
-      Patient pat1 = new Patient();
-      pat1.setId("1");
-      pat1.addIdentifier().setSystem("http://acme.com/MRNs").setValue("7000135");
-      pat1.addName().setFamily("Simpson").addGiven("Homer").addGiven("J");
-      myPatients.put("1", pat1);
-   }
+	/**
+	 * Constructor
+	 */
+	public Example01_PatientResourceProvider() {
+		Patient pat1 = new Patient();
+		pat1.setId("1");
+		pat1.addIdentifier().setSystem("http://acme.com/MRNs").setValue("7000135");
+		pat1.addName().setFamily("Simpson").addGiven("Homer").addGiven("J");
+		myPatients.put("1", pat1);
+	}
 
-   @Override
-   public Class<? extends IBaseResource> getResourceType() {
-      return Patient.class;
-   }
+	/* test */
+	@Override
+	public Class<? extends IBaseResource> getResourceType() {
+		return Patient.class;
+	}
 
-   /**
-    * Simple implementation of the "read" method
-    */
-   @Read()
-   public Patient read(@IdParam IdType theId) {
-      Patient retVal = myPatients.get(theId.getIdPart());
-      if (retVal == null) {
-         throw new ResourceNotFoundException(theId);
-      }
-      return retVal;
-   }
-
+	/**
+	 * Simple implementation of the "read" method
+	 */
+	@Read()
+	public Patient read(@IdParam IdType theId) {
+		Patient retVal = myPatients.get(theId.getIdPart());
+		if (retVal == null) {
+			throw new ResourceNotFoundException(theId);
+		}
+		return retVal;
+	}
 
 }
