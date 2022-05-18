c88e747e8337ad3a2ea923b306ed57a1288806d4
lmphatsi <la.mphatsi@gmail.com>
2022-05-18 16:48:15 +0300

    search functionality

--- java/hapi-fhirstarters-model-and-parser-examples/src/main/java/ca/uhn/fhir/example/Example08_ClientSearch.java
+++ java/hapi-fhirstarters-model-and-parser-examples/src/main/java/ca/uhn/fhir/example/Example08_ClientSearch.java
@@ -1,29 +1,31 @@
 package ca.uhn.fhir.example;
 
 import ca.uhn.fhir.rest.client.api.IGenericClient;
-import org.hl7.fhir.dstu3.model.Bundle;
-import org.hl7.fhir.dstu3.model.Patient;
+import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.Patient;
 
 import ca.uhn.fhir.context.FhirContext;
 
 public class Example08_ClientSearch {
-   public static void main(String[] theArgs) {
-      FhirContext ctx = FhirContext.forDstu3();
-      IGenericClient client = ctx.newRestfulGenericClient("http://fhirtest.uhn.ca/baseDstu3");
+	public static void main(String[] theArgs) {
+		FhirContext ctx = FhirContext.forR4();
+		IGenericClient client = ctx.newRestfulGenericClient("http://18.116.237.4:8080/fhir");
 
-      // Build a search and execute it
-      Bundle response = client.search()
-         .forResource(Patient.class)
-         .where(Patient.NAME.matches().value("Test"))
-         .and(Patient.BIRTHDATE.before().day("2014-01-01"))
-         .count(100)
-         .returnBundle(Bundle.class)
-         .execute();
+		// Build a search and execute it
+		Bundle response = client.search()
+				.forResource(Patient.class)
+				// .where(Patient.NAME.matches().value("Nairobi"))
+				// .and(Patient.BIRTHDATE.before().day("2014-01-01"))
+				// .count(100)
+				.returnBundle(Bundle.class)
+				.execute();
 
-      // How many resources did we find?
-      System.out.println("Responses: " + response.getTotal());
+		// How many resources did we find?
+		// System.out.println("Responses: " + response.getTotal());
+		System.out.println(ctx.newJsonParser().setPrettyPrint(true).encodeResourceToString(response));
 
-      // Print the ID of the first one
-      System.out.println("First response ID: " + response.getEntry().get(0).getResource().getId());
-   }
+		// Print the ID of the first one
+		// System.out.println("First response ID: " +
+		// response.getEntry().get(0).getResource().getId());
+	}
 }
